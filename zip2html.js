88c432de564cb3a024b0cb9870fb62d6311aa62f
guanyuan <yuanguan89@gmail.com>
2017-03-22 22:44:32 +0800

    handle response error of AdmZip

--- zip2html.js
+++ zip2html.js
@@ -1,25 +1,14 @@
 var http = require('http')
 var fs = require('fs')
 var AdmZip = require('adm-zip')
-var mkdirp = require('mkdirp');
 var jv = require('junit-viewer/src/main.js')
 
 
 var dir = "./assets/downloads";
 var filename = 'output'
-var url = 'http://localhost:8080/Archive.zip'
+var url = 'http://localhost:8080/homework.zip'
 var tmpFilePath =  dir + filename + ".zip"
 
-if (!fs.existsSync(dir)){
-  mkdirp(dir, function (err) {
-    console.log('mkdir finished')
-    if(err) {
-      console.log(err)
-    }
-  });
-} else {
-  console.log('no need to create new folder')
-}
 
 
 
@@ -31,8 +20,13 @@ http.get(url, function(response) {
       fs.appendFileSync(tmpFilePath, data)
     });
     response.on('end', function() {
-      var zip = new AdmZip(tmpFilePath)
-      zip.extractAllTo("assets/extracted/" + filename)
+
+      try{
+        var zip = new AdmZip(tmpFilePath)
+        zip.extractAllTo("assets/extracted/" + filename)
+      } catch(e) {
+        console.log('Exception: ' + e)
+      }
       //fs.unlink(tmpFilePath)
 
       var parsedAndRenderedData = jv.junit_viewer("assets/extracted/")
